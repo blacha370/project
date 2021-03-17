e53b0188d4aef5907eb8e9aeab485789a9a97123
blacha370 <lukasz.blacha370@gmail.com>
2021-03-17 21:41:30 +0100

    Added views and viewsets to urlpatterns

--- invoice/urls.py
+++ invoice/urls.py
@@ -1,6 +1,25 @@
 from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+router.register(r'address', views.AddressViewSet)
+router.register(r'company', views.CompanyViewSet)
+router.register(r'customer', views.CustomerViewSet)
+router.register(r'marketplace', views.MarketplaceViewSet)
+router.register(r'tax', views.TaxViewSet)
+router.register(r'item', views.ItemViewSet)
+router.register(r'sold_item', views.SoldItemViewSet)
+router.register(r'transaction', views.TransactionViewSet)
+router.register(r'receipt', views.ReceiptViewSet)
+router.register(r'invoice', views.InvoiceViewSet)
+router.register(r'advance_invoice', views.AdvanceInvoiceViewSet)
+
 urlpatterns = [
-    path('', views.hello_world)
+    path('', include(router.urls)),
+    path('add_customer', views.AddCustomer.as_view()),
+    path('create_tax', views.CreateTax.as_view()),
+    path('create_item', views.CreateItem.as_view()),
+    path('create_transaction', views.CreateTransaction.as_view())
 ]
