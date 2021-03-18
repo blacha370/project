544caa8d4dc6f582ca56767efa94f245cb51a383
blacha370 <lukasz.blacha370@gmail.com>
2021-03-18 21:14:36 +0100

    Added new views to urlpatterns

--- invoice/urls.py
+++ invoice/urls.py
@@ -21,5 +21,10 @@ urlpatterns = [
     path('add_customer', views.AddCustomer.as_view()),
     path('create_tax', views.CreateTax.as_view()),
     path('create_item', views.CreateItem.as_view()),
-    path('create_transaction', views.CreateTransaction.as_view())
+    path('create_transaction', views.CreateTransaction.as_view()),
+    path('create_receipt', views.CreateReceipt.as_view()),
+    path('create_invoice', views.CreateInvoice.as_view()),
+    path('create_advance_invoice', views.CreateAdvanceInvoice.as_view()),
+    path('end_invoice', views.EndInvoice.as_view()),
+    path('generate_sales_report/<int:year>/<int:month>', views.GenerateSalesReport.as_view())
 ]
